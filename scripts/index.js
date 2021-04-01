b058034ff22b1ecfa1d22b2bde212e6efe5702ee
Ksenia Onuprienko <kseniaonuprienko@yandex.ru>
2021-04-01 14:25:41 +0300

    Карточки через JS + popupAdd

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 let container = document.querySelector('.profile');
 let profileButton = container.querySelector('.profile__button');
 let popup = document.querySelector('.popup');
@@ -37,4 +36,69 @@ function formSubmitHandler(evt) {
   endClick();
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const element = document.querySelector('.elements');
+
+function createMestoCard(name, link) {
+  const template = document.querySelector('#mesto-card').content.cloneNode(true);
+  template.querySelector('.mesto-card__photo').src = link;
+  template.querySelector('.mesto-card__subtitle-name').textContent = name;
+  return template;
+};
+
+
+initialCards.forEach(function (card) {
+  element.prepend(createMestoCard(card.name, card.link));
+});
+
+let profileAdd = document.querySelector(".profile__button-add");
+let popupAdd = document.querySelector(".popup-add");
+let closeIconAdd = document.querySelector('.popup-add__close-icon');
+
+function showClickAdd() {
+  popupAdd.classList.add('popup-add_opened');
+  body.style.overflow = 'hidden';
+
+}
+
+profileAdd.addEventListener('click', showClickAdd);
+
+function endClickAdd() {
+  popupAdd.classList.remove('popup-add_opened');
+  body.style.overflow = 'visible';
+
+}
+
+closeIconAdd.addEventListener('click', endClickAdd);
+
+
+
+
