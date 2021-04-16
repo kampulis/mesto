87269bbefd4266270b49e88097df085fef07dd37
Ksenia Onuprienko <kseniaonuprienko@yandex.ru>
2021-04-16 22:55:52 +0300

    сделала валидацию

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const profile = document.querySelector('.profile');
 const profileButton = profile.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
-const body = document.querySelector('.body');
 const closeIcon = document.querySelector('.popup__close-icon');
 const formElement = document.querySelector('.popup__container_type_edit .popup__input');
 const formElementAddCard = document.querySelector('.popup_type_new-card .popup__input');
@@ -9,7 +8,6 @@ const nameInput = document.querySelector('#name');
 const jobInput = document.querySelector('#job');
 const profileTitle = document.querySelector('.profile__info-title');
 const profileSubtitle = document.querySelector('.profile__info-subtitle');
-const photo = document.querySelector('.photo');
 const profileButtonAdd = document.querySelector(".profile__button-add");
 const popupAdd = document.querySelector(".popup_type_new-card");
 const popupAddCloseButton = document.querySelector('.popup_type_new-card .popup__close-icon');
@@ -20,13 +18,13 @@ const fullPhoto = document.querySelector('.popup_type_image');
 const element = document.querySelector('.elements');
 const template = document.querySelector('#mesto-card');
 
-
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  resetFormsErrors();
 }
 
 function showClick() {
@@ -34,7 +32,6 @@ function showClick() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
-
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -95,6 +92,57 @@ function showPhoto(e) {
   document.querySelector('.popup__text').textContent = subtitle.textContent;
 }
 
+function validateInput(form, input, errorElement, formButton) {
+  const errorShort = `Минимальное количество символов ${input.minLength}. Длина текста сейчас: ${input.value.length} символ`;
+  if (input.validity.valueMissing) {
+    errorElement.textContent = "Вы пропустили это поле";
+    formButton.disabled = true;
+  } else if (input.validity.tooShort) {
+    errorElement.textContent = errorShort;
+    formButton.disabled = true;
+  } else if (input.validity.typeMismatch) {
+    errorElement.textContent = "Введите адрес сайта";
+    formButton.disabled = true;
+  } else {
+    errorElement.textContent = "";
+  }
+  if (form.checkValidity()) {
+    formButton.disabled = false;
+  }
+}
+
+function addValidationsToInputs(form, formButton) {
+  const formElements = Array.from(form.elements);
+  formElements.forEach(input => {
+    if (input.tagName === "INPUT") {
+      const errorElement = input.nextElementSibling;
+      input.addEventListener('input', (e) => {
+        e.preventDefault();
+        validateInput(form, e.target, errorElement, formButton);
+      });
+    }
+  })
+}
+
+function addValidationsToForms() {
+  const forms = Array.from(document.forms);
+  forms.forEach(form => {
+    const formButton = form.querySelector('.popup__button');
+    if (form.name === "new-card") {
+      formButton.disabled = true;
+    }
+    addValidationsToInputs(form, formButton);
+  });
+}
+
+function resetFormsErrors() {
+  const forms = Array.from(document.forms);
+  forms.forEach(form => {
+    const errorElements = form.querySelectorAll('.popup__name_type_err');
+    errorElements.forEach(element => element.textContent = "");
+  });
+}
+
 photoCloseButton.addEventListener('click', () => closePopup(fullPhoto));
 profileButton.addEventListener('click', showClick);
 closeIcon.addEventListener('click', () => closePopup(popup));
@@ -102,6 +150,7 @@ formElement.addEventListener('submit', formSubmitHandler);
 profileButtonAdd.addEventListener('click', () => openPopup(popupAdd));
 popupAddCloseButton.addEventListener('click', () => closePopup(popupAdd));
 popupAdd.addEventListener('submit', formSubmitAddMesto);
+addValidationsToForms();
 
 
 
